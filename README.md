7d6dadae9ae2878f8bbc96a06c71dc7c804a8f02
Arcania0311 <t.thomas.van.dam@gmail.com>
2014-06-03 12:43:15 +0200

    Initial code commit. Includes a small test text. Far form complete.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Visualizing Stucture
+# Visualising Structure
 
 An attempt at developing an application that is able to parse texts, identify cohesive ties within it, and then present the data collected in an undirected graph.
 
@@ -17,3 +17,9 @@ Since I intend to display the results in a graph it is probably best if I use th
 The parsing of the texts is of course the most difficult step and involves several stages:  
 * Identifying relevant words and keeping track of their count, and the relation between it and other relevant words.  
 * Identifying overlap in relevant words: two different words can refer to the same entity. An easy example are synonyms, but there are also pronouns to take into account.
+
+## Notes
+
+* Currently I strip all special characters from the tokens, which will be problematic with possessives and and/or constructions.  
+* I filter out common words ('a', 'the', etc) at the end. I'll want to change this when I add distance tracking.  
+* I have WordNet 3.0 working on my system. I feel that this tool can end up being really useful so I am going to dedicate some time to writing a wrapper for it in go.
---
+++ textParse.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+    "os"
+    "fmt"
+    "log"
+    "bufio"
+    "strings"
+)
+
+/*****
+ * Struct which stores details about a specific token.
+ */
+type wordToken struct {
+  id int
+  count int
+}
+
+/*****
+ * Open the specified file and return a slice of its lines.
+ */
+func importLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+func main() {
+  // Open the file.
+  lines, err := importLines("goTest.txt")
+  if err != nil {
+    log.Fatalf("readLines: %s", err)
+  }
+
+  // Create a map in which the words are stored.
+  id := 0
+  words := make(map[string]*wordToken)
+
+  // Process all the lines.
+  for _, line := range lines {
+    line = strings.ToLower(line)
+    splitLine := strings.Split(line, " ")
+
+    // Place all the words in the map with a corresponding id.
+    for _, tmp := range splitLine {
+
+      // Remove special characters from the string.
+      stripWord := func(r rune) rune {
+        switch {
+          case r < 'a' || r > 'z':
+            return -1
+        }
+        return r
+      }
+      tmp = strings.Map(stripWord, tmp)
+
+      token, ok := words[tmp]
+      if ok {
+        token.count++
+      } else {
+        words[tmp] = &wordToken{id, 1}
+        id++
+      }
+    }
+
+  }
+
+  // Ugly way to get rid of the most common words.
+  commonWords := make(map[string]bool)
+  commonWords["the"] = true
+  commonWords["a"] = true
+  commonWords["an"] = true
+  commonWords["is"] = true
+  commonWords["are"] = true
+  commonWords["of"] = true
+  commonWords["and"] = true
+  commonWords["be"] = true
+  commonWords["to"] = true
+  commonWords["in"] = true
+  commonWords["by"] = true
+
+  for key, value := range words {
+    if !commonWords[key] && value.count > 1 {
+      fmt.Println(value.count, key)
+    }
+  }
+}
